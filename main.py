16c6096d4239feffe3b11ec37dc878e31a927ba3
Dmitrii Khizbullin <d00533867@china.huawei.com>
2021-04-23 19:14:31 +0300

    Disabled dropout since it broke reproducibility

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
+        # self.dropout1 = nn.Dropout(0.25)
+        # self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
 
@@ -27,11 +27,11 @@ class Net(nn.Module):
         x = self.conv2(x)
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
+        # x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = F.relu(x)
-        x = self.dropout2(x)
+        # x = self.dropout2(x)
         x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
         return output
@@ -53,6 +53,8 @@ class DataparallelModel:
         self.models = models
         self.optimizers = optimizers
 
+        self.master_model_idx = 0
+
         if reference_model is not None:
             for param_group, ref_param in zip(self.param_group_gen(), reference_model.named_parameters()):
                 for param in param_group:
@@ -60,7 +62,7 @@ class DataparallelModel:
         else:
             for param_group in self.param_group_gen():
                 for param in param_group[1:]:
-                    param.data[...] = param_group[0].data[...]
+                    param.data[...] = param_group[self.master_model_idx].data[...]
 
         self.current_loss = None
 
@@ -69,46 +71,69 @@ class DataparallelModel:
         for group in zip(*param_groups):
             yield group
 
-    def step(self, data, target):
+    def step(self, data, target, dry_run=False):
+
+        assert len(data) % self.num_replicas == 0
+        offset = len(data) // self.num_replicas
 
         losses = []
         for i_replica, (model, optimizer) in enumerate(zip(self.models, self.optimizers)):
+            data_rep = data[i_replica*offset:(i_replica+1)*offset]
+            target_rep = target[i_replica*offset:(i_replica+1)*offset]
+            # if i_replica == 0:
+            #     print("Replica data:", data_rep.shape)
+            #     print(data_rep[0, 0, 15, ...])
             optimizer.zero_grad()
-            output = model(data)
-            loss = self.loss_functor(output, target)
+            output = model(data_rep)
+            loss = self.loss_functor(output, target_rep)
             loss.backward()
             losses.append(loss.item())
 
-        self.current_loss = np.mean(np.array(losses))
+        self.current_loss = np.sum(np.array(losses))
 
         # gradient allreduce here
 
-        for param_group in self.param_group_gen():
-            param_group_data = tuple(p.data for p in param_group)
-            sum_tensor = torch.sum(torch.stack(param_group_data, dim=0), dim=0)
-            for param in param_group:
-                param.data[...] = sum_tensor[...]
+        # for param_group in self.param_group_gen():
+        #     param_group_data = tuple(p.grad for p in param_group)
+        #     sum_tensor = torch.sum(torch.stack(param_group_data, dim=0), dim=0)
+        #     for param in param_group:
+        #         param.grad[...] = sum_tensor[...]
 
-        for i_replica, (model, optimizer) in enumerate(zip(self.models, self.optimizers)):
-            optimizer.step()
+        for param_group in self.param_group_gen():
+            print("------------ 1")
+            param_group_grad = tuple(p.grad for p in param_group)
+            for grad in param_group_grad:
+                print(grad[0, 0, ...])
+            print("------------ 2")
+            sum_tensor = torch.sum(torch.stack(param_group_grad, dim=0), dim=0)
+            print(sum_tensor[0, 0, ...])
+            print("------------ 3")
+            for grad in param_group_grad:
+                grad[...] = sum_tensor[...]
+                print(grad[0, 0, ...])
+            break
+
+        if not dry_run:
+            for i_replica, (model, optimizer) in enumerate(zip(self.models, self.optimizers)):
+                optimizer.step()
 
         # check all replica weights are identical
 
         return
 
     def get_loss(self):
-        return 0.0
+        return self.current_loss
 
     def named_gradients(self):
         assert len(self.models) > 0
         def grad_gen():
-            for param in self.models[0].named_parameters():
+            for param in self.models[self.master_model_idx].named_parameters():
                 yield param[0], param[1].data.grad
         return grad_gen()
 
     def named_parameters(self):
         assert len(self.models) > 0
-        return self.models[0].named_parameters()
+        return self.models[self.master_model_idx].named_parameters()
 
 
 class Trainer:
@@ -150,8 +175,11 @@ class Trainer:
         self.model = net_factory()
         self.optimizer = optimizer_factory(self.model.parameters())
 
+        from functools import partial
+        self.loss_func = partial(F.nll_loss, reduction="sum")
+
         num_replicas = 4
-        self.dataparallel_model = DataparallelModel(net_factory, optimizer_factory, F.nll_loss, num_replicas,
+        self.dataparallel_model = DataparallelModel(net_factory, optimizer_factory, self.loss_func, num_replicas,
                                                     reference_model=self.model)
 
         self.scheduler = StepLR(self.optimizer, step_size=1, gamma=args.gamma)
@@ -169,34 +197,55 @@ class Trainer:
 
     def train_epoch(self, epoch):
         self.model.train()
+
+        def _print_info():
+            for ref_np, dp_np in zip(self.model.named_parameters(), self.dataparallel_model.named_parameters()):
+                print(ref_np[0], dp_np[0])
+                # print("Weights:")
+                # print(ref_np[1].data[0, 0, ...])
+                # print(dp_np[1].data[0, 0, ...])
+                print("Grads:")
+                if ref_np[1].grad is not None:
+                    print(ref_np[1].grad[0, 0, ...])
+                else:
+                    print("None")
+                if dp_np[1].grad is not None:
+                    print(dp_np[1].grad[0, 0, ...])
+                else:
+                    print("None")
+                print("")
+                break
+
+        _print_info()
+
         for batch_idx, (data, target) in enumerate(self.train_loader):
             data, target = data.to(self.device), target.to(self.device)
 
-            self.optimizer.zero_grad()
-            output = self.model(data)
-            loss = F.nll_loss(output, target)
-            loss.backward()
-            self.optimizer.step()
+            dry_run = True
 
-            self.dataparallel_model.step(data, target)
+            self.optimizer.zero_grad()
+            # _num_replicas = 4
+            # i_replica = 0
+            # assert len(data) % _num_replicas == 0
+            # offset = len(data) // _num_replicas
+            # data_rep = data[i_replica*offset:(i_replica+1)*offset]
+            # target_rep = target[i_replica*offset:(i_replica+1)*offset]
+            # print("Regular data:", data_rep.shape)
+            # print(data_rep[0, 0, 15, ...])
+            output = self.model(data)
+            loss = self.loss_func(output, target)
+            loss.backward()
+            if not dry_run:
+                self.optimizer.step()
+
+            _print_info()
+
+            self.dataparallel_model.step(data, target, dry_run=dry_run)
             dp_loss = self.dataparallel_model.get_loss()
-            # dp_gradients = self.dataparallel_model.named_gradients()
-            dp_parameters = self.dataparallel_model.named_parameters()
 
             print(f"Loss, reference={loss.item()} dp={dp_loss}")
 
-            # for ref_np, dp_np in zip(self.model.named_parameters(), dp_parameters):
-            #     print(ref_np, dp_np)
-
-            # def grad_gen():
-            #     for param in self.models[0].parameters():
-            #         yield param.data.grad
-
-            for ref_np, dp_np in zip(self.model.named_parameters(), dp_parameters):
-                print(ref_np[0], dp_np[0])
-                print(ref_np[1].grad[0, 0, ...])
-                print(dp_np[1].grad[0, 0, ...])
-                print("")
+            _print_info()
 
             if batch_idx % self.args.log_interval == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
@@ -211,7 +260,7 @@ class Trainer:
             for data, target in self.test_loader:
                 data, target = data.to(self.device), target.to(self.device)
                 output = self.model(data)
-                test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+                test_loss += self.loss_func(output, target).item()  # sum up batch loss
                 pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
                 correct += pred.eq(target.view_as(pred)).sum().item()
 
